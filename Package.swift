739723d145a96ba176e982daa2876d4b26f85ecc
YPODDDD <yingpod_1997@hotmail.com>
2025-04-23 10:02:04 +0700

    add new

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
     name: "MaterialComponents",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v13)
     ],
     products: [
         .library(
